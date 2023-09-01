e046067fa768427f691a7457ca19b4186145b095
Oleh_K <oleh.usa2017@gmail.com>
2023-09-01 13:59:35 -0400

    Spring-05-Core-BeanScope

--- Spring-05-Core-BeanScope/src/main/java/com/messenger/MessengerApp.java
+++ Spring-05-Core-BeanScope/src/main/java/com/messenger/MessengerApp.java
@@ -3,8 +3,12 @@ package com.messenger;
 import com.messenger.configuration.ProjectConfig;
 import com.messenger.model.Comment;
 import com.messenger.service.CommentService;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import java.util.Objects;
 
 
 public class MessengerApp {
@@ -13,9 +17,16 @@ public class MessengerApp {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        CommentService commentService = applicationContext.getBean(CommentService.class);
-        commentService.publishComment(comment);
-        System.out.println("===========================================");
+
+//        CommentService bean_1 = applicationContext.getBean(CommentService.class);
+//        CommentService bean_2 = applicationContext.getBean(CommentService.class);
+//
+//        Objects will be same unless u will add -> @Scope("prototype") or @Scope(BeanDefinition.SCOPE_PROTOTYPE) in Comment service class
+//        System.out.println("bean_1 = " + bean_1);
+//        System.out.println("bean_2 = " + bean_2);
+//        System.out.println(bean_1 == bean_2);
+//        System.out.println("===============================================");
+
 
     }
 }
--- Spring-05-Core-BeanScope/src/main/java/com/messenger/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/messenger/service/CommentService.java
@@ -4,17 +4,23 @@ import com.messenger.model.Comment;
 import com.messenger.proxy.CommentNotificationProxy;
 import com.messenger.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component // this class has dependency
-
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+//@Lazy // lazy instantiation by default it eager. Lazy -> crete object when call it only.
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService( @Qualifier("PostgreSQL")CommentRepository commentRepository,@Qualifier("androidPushCommentNotification") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(@Qualifier("PostgreSQL") CommentRepository commentRepository, @Qualifier("androidPushCommentNotification") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Comment service created");
     }
 
     public void publishComment(Comment comment) {
