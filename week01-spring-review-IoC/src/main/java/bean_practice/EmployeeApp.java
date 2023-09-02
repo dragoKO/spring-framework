5f0b2b9aff2ed403d3d0bff8ce208cd73d39064f
Oleh_K <oleh.usa2017@gmail.com>
2023-09-02 11:56:47 -0400

    week01-spring-review-IoC

--- week01-spring-review-IoC/src/main/java/bean_practice/EmployeeApp.java
+++ week01-spring-review-IoC/src/main/java/bean_practice/EmployeeApp.java
@@ -5,13 +5,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class EmployeeApp {
     public static void main(String[] args) {
-        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class, AppConfig2.class);
 
         FullTimeEmployee fullTimeEmployee = applicationContext.getBean(FullTimeEmployee.class);
         PartTimeEmployee partTimeEmployee = applicationContext.getBean(PartTimeEmployee.class);
 
         fullTimeEmployee.createAccount();
         partTimeEmployee.createAccount();
+
+        String welcomeMessage = applicationContext.getBean("welcomeMessage",String.class);
+        System.out.println(welcomeMessage);
+
+        String springCoreMessage = applicationContext.getBean("springCoreMessage",String.class);
+        System.out.println(springCoreMessage);
 
     }
 }
