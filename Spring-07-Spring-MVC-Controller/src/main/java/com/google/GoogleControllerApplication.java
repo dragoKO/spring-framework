069dd6d17b34b7aa3534a16308b0e5d9e1a61060
Oleh_K <oleh.usa2017@gmail.com>
2023-09-07 12:35:05 -0400

    Spring-07-Spring-MVC-Controller

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/google/GoogleControllerApplication.java
@@ -0,0 +1,13 @@
+package com.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleControllerApplication.class, args);
+    }
+
+}
