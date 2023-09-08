94548fa4bc9863c558c2a47a20bd3a59113d2b89
Oleh_K <oleh.usa2017@gmail.com>
2023-09-08 14:06:33 -0400

    Spring_08_Spring_MVC_Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/Spring08SpringMvcModelApplication.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/Spring08SpringMvcModelApplication.java
@@ -1,13 +1,13 @@
-package com.google;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoogleModelApplication {
+public class Spring08SpringMvcModelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GoogleModelApplication.class, args);
+        SpringApplication.run(Spring08SpringMvcModelApplication.class, args);
     }
 
 }
--- Spring-08-Spring-MVC-Model/src/test/java/com/cydeo/Spring08SpringMvcModelApplicationTests.java
+++ Spring-08-Spring-MVC-Model/src/test/java/com/cydeo/Spring08SpringMvcModelApplicationTests.java
@@ -1,10 +1,10 @@
-package com.google;
+package com.cydeo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GoogleModelApplicationTests {
+class Spring08SpringMvcModelApplicationTests {
 
     @Test
     void contextLoads() {
