913f2c3ce5faeeba48461dedb30bdaa4bd42f716
Oleh_K <oleh.usa2017@gmail.com>
2023-11-11 22:31:10 -0500

    run successfully

--- Spring-13-ORM-CinemaLab/src/main/java/com/cinema/spring13ormcinemalab/CinemaApp.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cinema/spring13ormcinemalab/CinemaApp.java
@@ -1,13 +1,23 @@
 package com.cinema.spring13ormcinemalab;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CinemaApp {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaApp.class, args);
+    }
+
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
 
 }
