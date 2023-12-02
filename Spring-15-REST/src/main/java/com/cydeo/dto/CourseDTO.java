46970cbe6c6fb0b587b3b6057e8fae4d05f9fda9
Oleh_K <oleh.usa2017@gmail.com>
2023-12-02 18:55:23 -0500

    project fix

---
+++ Spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -0,0 +1,28 @@
+package com.cydeo.dto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CourseDTO {
+
+
+    private Long id;
+
+
+    private String name;
+
+
+    private String category;
+
+
+    private int rating;
+
+
+    private String description;
+
+}
